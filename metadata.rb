f2b3c2f3c8051e7958054707d1cf624066193c23
782112 <d.webb@derby.ac.uk>
2014-03-18 12:21:00 +0000

    -a

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
-name             "artifactory"
-maintainer       "Avishai Ish-Shalom"
-maintainer_email "avishai@fewbytes.com"
-license          "Apache V2"
-description      "Installs/Configures artifactory"
+name             'artifactory'
+maintainer       'Avishai Ish-Shalom'
+maintainer_email 'avishai@fewbytes.com'
+license          'Apache V2'
+description      'Installs/Configures artifactory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          '0.1.1'
 
-depends          "java"
-depends          "runit"
-depends          "ark"
-depends          "apache2"
+depends          'java'
+depends          'runit'
+depends          'ark'
+depends          'apache2'
 
-supports "ubuntu"
-supports "debian"
+supports 'ubuntu'
+supports 'debian'
